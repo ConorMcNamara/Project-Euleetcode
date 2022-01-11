25b8563ef60d8526a233ba579a937d7956ad83ee
Conor McNamara <conor.s.mcnamara@gmail.com>
2022-01-11 13:16:05 -0800

    Update 197_risingTemperature.sql

--- Leetcode/SQL/197_risingTemperature.sql
+++ Leetcode/SQL/197_risingTemperature.sql
@@ -1,4 +1,14 @@
-select w1.id
-from weather w1, weather w2
-where datediff(w1.recorddate, w2.recorddate) = 1
-and w1.temperature > w2.temperature
+SELECT
+    w1.id
+FROM weather w1, weather w2
+WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1
+AND w1.temperature > w2.temperature
+
+--Alternatively
+SELECT
+    w1.id
+FROM weather w1
+JOIN weather w2
+ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+WHERE
+    w1.temperature > w2.temperature
