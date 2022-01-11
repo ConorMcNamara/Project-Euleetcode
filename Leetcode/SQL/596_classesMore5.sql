53ee4b3cf15c1e3bbf5f16353661112b079d9000
Conor McNamara <conor.s.mcnamara@gmail.com>
2022-01-11 13:18:35 -0800

    Update 596_classesMore5.sql

--- Leetcode/SQL/596_classesMore5.sql
+++ Leetcode/SQL/596_classesMore5.sql
@@ -1,7 +1,6 @@
-select a.class
-from (select student, class
-      from courses
-      group by student, class
-      having count(*) = 1) a
-      group by a.class
-      having count(a.class) >= 5
+SELECT 
+    class
+FROM courses
+GROUP BY 
+    class
+HAVING COUNT(*) >= 5
