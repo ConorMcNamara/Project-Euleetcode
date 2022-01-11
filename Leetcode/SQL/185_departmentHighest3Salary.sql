ad150698c391476e19ec3a974a3fc45378aa1f40
Conor McNamara <conor.s.mcnamara@gmail.com>
2022-01-11 13:14:41 -0800

    Update 185_departmentHighest3Salary.sql

--- Leetcode/SQL/185_departmentHighest3Salary.sql
+++ Leetcode/SQL/185_departmentHighest3Salary.sql
@@ -1,5 +1,19 @@
-Select dep.name as Department, emp.name as Employee, emp.salary from
-Department dep,
-Employee emp
-where emp.departmentid=dep.id and
-(select count(distinct salary) from Employee where departmentid=dep.id and salary > emp.salary)<3
+WITH salary_ranks AS (
+SELECT
+    departmentId,
+    salary,
+    DENSE_RANK() OVER (partition by departmentId ORDER BY salary DESC) AS sal_ranks
+FROM employee
+)
+SELECT DISTINCT
+    department.name AS Department,
+    employee.name AS Employee,
+    salary_ranks.salary AS Salary
+FROM employee
+JOIN department
+    ON employee.departmentId = department.id
+JOIN salary_ranks
+    ON employee.departmentId = salary_ranks.departmentId
+    AND employee.salary = salary_ranks.salary
+WHERE
+    sal_ranks <= 3
