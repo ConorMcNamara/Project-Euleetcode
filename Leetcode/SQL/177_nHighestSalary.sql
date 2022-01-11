c1daec3f84e4ae72f56501d91c662fbdc213d474
Conor McNamara <conor.s.mcnamara@gmail.com>
2022-01-11 13:06:03 -0800

    Update 177_nHighestSalary.sql

--- Leetcode/SQL/177_nHighestSalary.sql
+++ Leetcode/SQL/177_nHighestSalary.sql
@@ -2,9 +2,25 @@ CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   set N=N-1;
   RETURN (
-      select distinct(salary) as salary
-      from employee
-      order by salary desc
-      limit 1 offset N
+      SELECT DISTINCT(salary) as salary
+      FROM employee
+      ORDER BY salary DESC
+      LIMIT 1 offset N
+  );
+END
+
+--Alternatively
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      WITH salary_rank AS (
+                          SELECT salary,
+                          dense_rank() OVER (order by salary DESC) as rank_sal
+                          FROM employee
+      )
+      SELECT salary
+      FROM salary_rank
+      WHERE rank_sal = N
+      LIMIT 1
   );
 END
