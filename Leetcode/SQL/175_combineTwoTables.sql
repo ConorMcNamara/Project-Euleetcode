948d6cc6c61f09df08566df11d1f745f4ffbc156
Conor McNamara <conor.s.mcnamara@gmail.com>
2022-01-11 13:02:26 -0800

    Update 175_combineTwoTables.sql

--- Leetcode/SQL/175_combineTwoTables.sql
+++ Leetcode/SQL/175_combineTwoTables.sql
@@ -1,4 +1,11 @@
-select a.firstname, a.lastname, b.city, b.state
-from person a
-LEFT JOIN address b
-on a.personid = b.personid
+SELECT 
+    firstName,
+    lastName,
+    city,
+    state
+FROM 
+    Person
+LEFT JOIN
+    Address
+ON 
+    Person.personId = Address.personId
